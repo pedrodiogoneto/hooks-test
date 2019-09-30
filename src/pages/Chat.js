528239c8e513b49cef99ed31f05cd6aee012d959
pedrodiogoneto <pedritoneto@gmail.com>
2019-09-30 14:50:32 +0200

    Add UserSelector component

---
+++ src/pages/Chat.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import UserSelector from '../components/UserSelector'
+
+const Chat = () => {
+	const [showUserSelector, setShowUserSelector] = useState(true);
+
+	return (
+		<React.Fragment>
+			{ showUserSelector && <UserSelector /> }
+			<h1>Parent component</h1>
+		</React.Fragment>
+	)
+}
+
+export default Chat
